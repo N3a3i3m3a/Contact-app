a23441ae4f677ae95c92afede50bb460e49700b2
N3a3i3m3a <nishimwenaima3@gmail.com>
2024-04-07 15:58:00 +0200

    done with list

--- src/components/addContact.jsx
+++ src/components/addContact.jsx
@@ -1,40 +1,36 @@
-import React from 'react'
-import NavBar from './navBar'
+import React, { Fragment } from 'react'; // Import Fragment from react
+import NavBar from './navBar';
 
 const AddContact = () => {
   return (
-    <fragment>
-      <NavBar/>
-      <section>
-        <h2>Add contact</h2>
-        <div className='addform'>
-        <div className=''>
-  <h3>Full name</h3>
-  <input type='text' className='border border-black'/>
-</div>
-<div className=''>
-  <h3>Email</h3>
-  <input type='text' className='border border-black'/>
-</div>
-<div className=''>
-  <h3>Phone</h3>
-  <input type='text' className='border border-black'/>
-</div>
-<div className=''>
-  <h3>Picture</h3>
-  <input type='text' className='border border-black'/>
-</div>
-<div>
-  <button className='bg-gray-400'>Create</button>
-</div>
-
-
+    <Fragment>
+      <NavBar />
+      <section className="px-4 lg:px-20 py-8">
+        <h2 className="text-2xl lg:text-3xl font-semibold mb-8">Add Contact</h2>
+        <div className="flex flex-col lg:flex-row gap-4">
+          <div className="flex flex-col w-full lg:w-1/4">
+            <h3 className="mb-2">Full name</h3>
+            <input type="text" className="border border-black rounded-lg p-2" />
+          </div>
+          <div className="flex flex-col w-full lg:w-1/4">
+            <h3 className="mb-2">Email</h3>
+            <input type="text" className="border border-black rounded-lg p-2" />
+          </div>
+          <div className="flex flex-col w-full lg:w-1/4">
+            <h3 className="mb-2">Phone</h3>
+            <input type="text" className="border border-black rounded-lg p-2" />
+          </div>
+          <div className="flex flex-col w-full lg:w-1/4">
+            <h3 className="mb-2">Picture</h3>
+            <input type="text" className="border border-black rounded-lg p-2" />
+          </div>
         </div>
-
+        <div className="mt-6">
+          <button className="bg-gray-400 py-2 px-4 rounded-lg">Create</button>
+        </div>
       </section>
+    </Fragment>
+  );
+};
 
-    </fragment>
-  )
-}
-
-export default AddContact
+export default AddContact;
--- src/components/contactList.jsx
+++ src/components/contactList.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import { IoIosMore } from "react-icons/io";
+import { FaEye } from "react-icons/fa";
+import { CiEdit } from "react-icons/ci";
+import { MdDelete } from "react-icons/md";
 const ContactList = () => {
   return (
     <fragment className='flex flex-col'>
@@ -15,39 +17,52 @@ const ContactList = () => {
       <section className='contact-list'>
          <div className='bg-gray-400 py-7 mb-3 flex flex-row justify-between'>
             <div className=''>
-             <h3>Diane</h3>
-             <h3>07866666666</h3>
+             <h3>Name: Diane</h3>
+             <h3>Phone: 07866666666</h3>
+             <h3>Email: dana@gmail.com</h3>
             </div>
             <div className=''>
-              <Link to={'/'} className=''>
-              <IoIosMore />
-              </Link>
-              <h3>dana@gmail.com</h3>
-            </div>
+              <Link to={'/'} className=''><FaEye /></Link>
+              </div>
+              <div className=''>
+              <Link to={'/'} className=''><CiEdit /></Link>
+              </div>
+              <div className=''>
+              <Link to={'/'} className=''><MdDelete /></Link>
+              </div>
+              
          </div>
          <div className='bg-gray-400 py-7 mb-3 flex flex-row justify-between'>
             <div className=''>
-             <h3>Ritha</h3>
-              <h>0787777777777</h>
+             <h3>Name: Ritha</h3>
+              <h3>Phone: 0787777777777</h3>
+              <h3>Eail: ritha@gmail.com</h3>
              </div>
              <div className=''>
-             <Link to={'/'} className=''>
-             <IoIosMore />
-              </Link>
-              <h>ritha@gmail.com</h>
-            </div>
+              <Link to={'/'} className=''><FaEye /></Link>
+              </div>
+              <div className=''>
+              <Link to={'/'} className=''><CiEdit /></Link>
+              </div>
+              <div className=''>
+              <Link to={'/'} className=''><MdDelete /></Link>
+              </div>
          </div>
          <div className='bg-gray-400 py-7 mb-3 flex flex-row justify-between'>
           <div className=''>
-           <h3>Naima</h3>
-           <h>0788888888</h>
+           <h3>Name: Naima</h3>
+           <h3>Phone: 0788888888</h3>
+           <h3>Email: naima@gmail.com </h3>
            </div>
            <div className=''>
-           <Link to={'/'} className=''>
-           <IoIosMore />
-              </Link>
-              naima@gmail.com 
-            </div>
+              <Link to={'/contacts/details/:contactId'} className=''><FaEye /></Link>
+              </div>
+              <div className=''>
+              <Link to={'/contacts/edit/:contactId'} className=''><CiEdit /></Link>
+              </div>
+              <div className=''>
+              <Link to={'/'} className=''><MdDelete /></Link>
+              </div>
          </div>
       </section>
     </fragment>
